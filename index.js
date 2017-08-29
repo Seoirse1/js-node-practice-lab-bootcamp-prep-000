9ab86f3e187c6477a41023d433d0b6a4e44e2607
Seoirse Mac an Garda <domhaine.nua@gmail.com>
2017-08-29 07:34:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var name = "Seoirse"
 var height = "74"
+var height_as_number = 74
 
 console.log("Name:")
 console.log(name)
@@ -11,3 +12,6 @@ console.log(height)
 
 console.log("Height+1:")
 console.log(parseInt(height) + 1 )
+
+console.log("Height_as_number:")
+console.log(height + 1 )
